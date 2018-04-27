3cce7d268e4b0ea36d100af993039a92d5a7b50c
Guita1538 <domiccover@users.noreply.github.com>
2018-04-27 14:20:31 +0700

    Helllo world

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
-import React, { Component } from 'react';
-
 class App extends Component {
   render() {
     return (
-      <div>Hello</div>
+-      <div>Hello</div>
++      <div>Hello World</div>
     );
   }
 }
-
-export default App;
